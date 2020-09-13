5852d2ed4d7bfe1746240a21140cf35de5167787
Aleksander Kazmierczak <aleksander.kazmierczak@gmail.com>
2020-09-13 17:48:56 +0200

    Updates

---
+++ precache-manifest.47d386f407181a8c01120e51ec2dd739.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7feed6a48d941915174394265e98c173",
+    "url": "/al3kaz/Weather-App.git/index.html"
+  },
+  {
+    "revision": "9a6864fc0fe56b95989d",
+    "url": "/al3kaz/Weather-App.git/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "ccc8be9fc14c589f545d",
+    "url": "/al3kaz/Weather-App.git/static/css/main.bbea3685.chunk.css"
+  },
+  {
+    "revision": "9a6864fc0fe56b95989d",
+    "url": "/al3kaz/Weather-App.git/static/js/2.3c1459a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/al3kaz/Weather-App.git/static/js/2.3c1459a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccc8be9fc14c589f545d",
+    "url": "/al3kaz/Weather-App.git/static/js/main.486c0166.chunk.js"
+  },
+  {
+    "revision": "3a912fab5bbfc4cdab2f",
+    "url": "/al3kaz/Weather-App.git/static/js/runtime-main.ab535756.js"
+  },
+  {
+    "revision": "3817133dd616136c83b93b072c1fec07",
+    "url": "/al3kaz/Weather-App.git/static/media/bg.3817133d.jpg"
+  }
+]);
